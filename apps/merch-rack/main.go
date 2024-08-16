e541a5930be2921536c839777a5a7aa49c922f58
Dean <deanorhan@gmail.com>
2024-08-15 20:54:34 -0700

    move merch functions out into own file

--- apps/merch-rack/main.go
+++ apps/merch-rack/main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/deanorhan/merch-booth/libs/db-package"
 	"github.com/gin-gonic/gin"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -19,50 +13,5 @@ func main() {
 		merchGroup.POST("/", PostMerch)
 	}
 
-	// r.GET("/api/merch", GetMerch)
-	// r.POST("/api/merch", PostMerch)
 	r.Run() // listen and serve on 0.0.0.0:8080
-}
-
-type Merch struct {
-	Status int8    `json:"status"`
-	Title  string  `json:"title"`
-	Price  float32 `json:"price"`
-}
-
-func GetMerch(c *gin.Context) {
-	dab := GetDBConnection()
-
-	queries := db.New(dab)
-
-	merch, err := queries.ListMerch(c)
-	if err != nil {
-		log.Println(err)
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-		return
-	}
-
-	c.IndentedJSON(http.StatusOK, merch)
-}
-
-func PostMerch(c *gin.Context) {
-	var merch Merch
-
-	if err := c.BindJSON(&merch); err != nil {
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-		return
-	}
-
-	dab := GetDBConnection()
-
-	queries := db.New(dab)
-
-	m, err := queries.CreateMerch(c, db.CreateMerchParams{Vendor: 0, Status: 0, Title: merch.Title, Price: float64(merch.Price)})
-	if err != nil {
-		log.Println(err)
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-		return
-	}
-
-	c.IndentedJSON(http.StatusCreated, Merch{Status: int8(m.Status), Title: m.Title, Price: float32(m.Price)})
 }
