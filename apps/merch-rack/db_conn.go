ebfda33f2909cc93ee19a76fce60278875c409b5
Dean <deanorhan@gmail.com>
2024-08-17 14:14:42 -0700

    move to using functions for opening/closing the db instead of init

--- apps/merch-rack/db_conn.go
+++ apps/merch-rack/db_conn.go
@@ -7,13 +7,16 @@ import (
 
 var conn *sql.DB
 
-func init() {
+func Open() error {
+	log.Print("Opening the DB")
 	var err error
-
 	conn, err = sql.Open("sqlite3", "./merch.db")
-	if err != nil {
-		log.Panic(err)
-	}
+	return err
+}
+
+func Close() {
+	log.Print("Closing the DB")
+	conn.Close()
 }
 
 func GetDBConnection() *sql.DB {
--- apps/merch-rack/main.go
+++ apps/merch-rack/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	err := Open()
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	defer Close()
+
 	r := getRouter()
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
 
 func getRouter() *gin.Engine {
